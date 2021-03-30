18f70bb41f5d13b6f4e31128007961e920a913e3
hitika22 <iib2019021@iiita.ac.in>
2021-03-30 10:05:20 +0530

    flutter_backend

---
+++ swe_flutter/flu/lib/adminScreen.dart
@@ -0,0 +1,117 @@
+import 'package:demo_test_app/developer.dart';
+import 'package:flutter/material.dart';
+
+
+
+class AdminScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Stack(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('images/jii.jpg'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+
+
+              SafeArea(
+                child: Padding(
+                  padding: const EdgeInsets.all(200.0),
+                  child: Container(
+                    child: Expanded(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment:CrossAxisAlignment.stretch,
+                        children: [
+
+                          FlatButton(
+
+                            onPressed:(){
+                              Navigator.push(context, MaterialPageRoute(builder: (context) =>Devop()));
+                            },
+                            hoverColor: Colors.blueGrey,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text('ADD/UPDATE DOCTOR', style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                // backgroundColor: Colors.black,
+                              )
+                              ),
+                            ),
+                            textColor: Colors.white,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                              color: Colors.white70,
+                              width: 5,
+                              style: BorderStyle.solid,
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+
+
+                          FlatButton(
+
+                            onPressed:(){
+
+                            },
+                            hoverColor: Colors.blueGrey,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text('MODIFY SOFTWARE', style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                // backgroundColor: Colors.black,
+                              )
+                              ),
+                            ),
+                            textColor: Colors.black,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                              color: Colors.white70,
+                              width: 5,
+                              style: BorderStyle.solid,
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+
+                          FlatButton(
+
+                            onPressed:(){
+
+                            },
+                            hoverColor: Colors.blueGrey,
+                            child: Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text('MODIFY MODEL', style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 30,
+                                // backgroundColor: Colors.black,
+                              )
+                              ),
+                            ),
+                            textColor: Colors.white,
+                            shape: RoundedRectangleBorder(side: BorderSide(
+                              color: Colors.white70,
+                              width: 5,
+                              style: BorderStyle.solid,
+                            ), borderRadius: BorderRadius.circular(50)),
+                          ),
+
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ swe_flutter/flu/lib/testing.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Doneit extends StatefulWidget {
+  @override
+  _DoneitState createState() => _DoneitState();
+}
+
+class _DoneitState extends State<Doneit> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('didit'),
+      ),
+
+    );
+  }
+}
