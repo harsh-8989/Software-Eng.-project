97caacc415c07910f453ad9e3dd36ac59bf506c9
hitika22 <70220300+hitika22@users.noreply.github.com>
2021-03-31 23:39:14 +0530

    Update developer.dart

--- swe_flutter/flu/lib/developer.dart
+++ swe_flutter/flu/lib/developer.dart
@@ -23,74 +23,109 @@ class _DevopState extends State<Devop> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('I AM A DEVELOPER'),
+        backgroundColor: Colors.black,
+        title: Center(
+          child: Text('I AM A DEVELOPER',
+          style: TextStyle(
+            color: Colors.white
+          ),
+          ),
+        ),
       ),
-      body:Container(
-        child: StreamBuilder(
-            stream: userRef.snapshots(),
-            builder: (BuildContext context,
-                AsyncSnapshot<QuerySnapshot> snapshot) {
-              if (snapshot.hasError) {
-                return Text("Something went wrong");
-              }
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return CircularProgressIndicator();
-              }
-              return ListView(
-                children: snapshot.data.docs.map(
-                      (document) {
-                    return
-                      //   CustomUserTile(
-                      //   document.id,
-                      //   title: document["name"] + "(" + document.id + ")",
-                      //   description: document["description"],
-                      //   isEnroled: enroled.contains(document.id),
-                      // )
-                      Container(
-                        child: Column(
-                          children: [
-                            Text(
-                                document["name"],
-                            style: TextStyle(
-                            color: Colors.deepPurple,
-                            fontSize: 32.0,
+      body:Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[
+                      Color(0xFF6E14FF),
+                      Color(0xFF5F5F5F)
+                    ])
+            ),
+          ),
+          Container(
+            child: StreamBuilder(
+                stream: userRef.snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError) {
+                    return Text("Something went wrong");
+                  }
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return CircularProgressIndicator();
+                  }
+                  return ListView(
+                    children: snapshot.data.docs.map(
+                          (document) {
+                        return
+                          //   CustomUserTile(
+                          //   document.id,
+                          //   title: document["name"] + "(" + document.id + ")",
+                          //   description: document["description"],
+                          //   isEnroled: enroled.contains(document.id),
+                          // )
+                          Container(
 
-                            ),
-                            ),
-                            Text(
-                              document["email"],
-                              style: TextStyle(
-                                color: Colors.deepPurple,
-                                fontSize: 32.0,
-
+                            child: Container(
+                              padding: EdgeInsets.all(8),
+                              margin: EdgeInsets.all(8),
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                color: Colors.white30,
                               ),
-                            ),Text(
-                              document["phonenumber"],
-                              style: TextStyle(
-                                color: Colors.deepPurple,
-                                fontSize: 32.0,
 
+                              child: Column(
+                                children: [
+
+                                  Text(
+                                    document["name"],
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w900,
+                                      fontSize: 25.0,
+
+                                    ),
+                                  ),
+                                  Text(
+                                    document["email"],
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25.0,
+
+                                    ),
+                                  ),Text(
+                                    document["phonenumber"],
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 25.0,
+
+                                    ),
+                                  ),
+
+                                  RaisedButton(
+                                    child: Text(document["isdoct"].toString()),
+                                    onPressed: (){
+                                      changestatus(document["id"],document["isdoct"]);
+                                    },
+                                  ),
+                                  SizedBox(
+                                    height:25,
+                                  ),
+
+                                ],
                               ),
                             ),
+                          );
+                      },
+                    ).toList(),
+                  );
+                }),
+          ),
+        ],
+      )
 
-                            RaisedButton(
-                              child: Text(document["isdoct"].toString()),
-                              onPressed: (){
-                                changestatus(document["id"],document["isdoct"]);
-                              },
-                            ),
-                            SizedBox(
-                              height:25,
-                            ),
-
-                          ],
-                        ),
-                      );
-                  },
-                ).toList(),
-              );
-            }),
-      ),
     );
   }
 }
