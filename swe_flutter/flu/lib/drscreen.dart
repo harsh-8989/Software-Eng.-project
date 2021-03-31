aeaae10baea8010cd2c04b8d103051248b39ed6f
hitika22 <70220300+hitika22@users.noreply.github.com>
2021-04-01 00:16:35 +0530

    Update drscreen.dart

--- swe_flutter/flu/lib/drscreen.dart
+++ swe_flutter/flu/lib/drscreen.dart
@@ -4,109 +4,123 @@ import 'package:flutter/material.dart';
 class DrScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: SafeArea(
-          child: Stack(
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage('images/hi.jpg'),
-                    fit: BoxFit.cover,
-                  ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Center(
+          child:
+          Center(
+            child: Text(
+                "WELCOME",
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w900,
+                )
+            ),
+          ),
+
+        ),
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/hi.jpg'),
+                  fit: BoxFit.cover,
                 ),
               ),
+            ),
 
 
-              SafeArea(
-                child: Padding(
-                  padding: const EdgeInsets.all(200.0),
-                  child: Container(
-                    child: Expanded(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        crossAxisAlignment:CrossAxisAlignment.stretch,
-                        children: [
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(200.0),
+                child: Container(
+                  child: Expanded(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
 
-                          FlatButton(
+                        FlatButton(
 
-                            onPressed:(){
+                          onPressed: () {
 
-                            },
-                            hoverColor: Colors.blueGrey,
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text('VISUALIZE MODEL', style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 30,
-                                // backgroundColor: Colors.black,
-                              )
-                              ),
+                          },
+                          hoverColor: Colors.blueGrey,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text('VISUALIZE MODEL', style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                              // backgroundColor: Colors.black,
+                            )
                             ),
-                            textColor: Colors.white,
-                            shape: RoundedRectangleBorder(side: BorderSide(
-                              color: Colors.white70,
-                              width: 5,
-                              style: BorderStyle.solid,
-                            ), borderRadius: BorderRadius.circular(50)),
                           ),
+                          textColor: Colors.white,
+                          shape: RoundedRectangleBorder(side: BorderSide(
+                            color: Colors.white70,
+                            width: 5,
+                            style: BorderStyle.solid,
+                          ), borderRadius: BorderRadius.circular(50)),
+                        ),
 
 
-                          FlatButton(
+                        FlatButton(
 
-                            onPressed:(){
+                          onPressed: () {
 
-                            },
-                            hoverColor: Colors.blueGrey,
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text('MODIFY SOFTWARE', style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 30,
-                                // backgroundColor: Colors.black,
-                              )
-                              ),
+                          },
+                          hoverColor: Colors.blueGrey,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text('MODIFY SOFTWARE', style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                              // backgroundColor: Colors.black,
+                            )
                             ),
-                            textColor: Colors.black,
-                            shape: RoundedRectangleBorder(side: BorderSide(
-                              color: Colors.white70,
-                              width: 5,
-                              style: BorderStyle.solid,
-                            ), borderRadius: BorderRadius.circular(50)),
                           ),
+                          textColor: Colors.black,
+                          shape: RoundedRectangleBorder(side: BorderSide(
+                            color: Colors.white70,
+                            width: 5,
+                            style: BorderStyle.solid,
+                          ), borderRadius: BorderRadius.circular(50)),
+                        ),
 
-                          FlatButton(
+                        FlatButton(
 
-                            onPressed:(){
+                          onPressed: () {
 
-                            },
-                            hoverColor: Colors.blueGrey,
-                            child: Padding(
-                              padding: const EdgeInsets.all(16.0),
-                              child: Text('CONTACT DEVELOPER', style: TextStyle(
-                                color: Colors.black,
-                                fontSize: 30,
-                                // backgroundColor: Colors.black,
-                              )
-                              ),
+                          },
+                          hoverColor: Colors.blueGrey,
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Text('CONTACT DEVELOPER', style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 30,
+                              // backgroundColor: Colors.black,
+                            )
                             ),
-                            textColor: Colors.white,
-                            shape: RoundedRectangleBorder(side: BorderSide(
-                              color: Colors.white70,
-                              width: 5,
-                              style: BorderStyle.solid,
-                            ), borderRadius: BorderRadius.circular(50)),
                           ),
-                        ],
-                      ),
+                          textColor: Colors.white,
+                          shape: RoundedRectangleBorder(side: BorderSide(
+                            color: Colors.white70,
+                            width: 5,
+                            style: BorderStyle.solid,
+                          ), borderRadius: BorderRadius.circular(50)),
+                        ),
+                      ],
                     ),
                   ),
                 ),
               ),
+            ),
 
-            ],
-          ),
+          ],
         ),
       ),
     );
