060147dc1dd04002da4fe2ba98e0b323a1937333
harsh-8989 <harshmahajan8989@gmail.com>
2021-04-02 18:00:23 +0530

    t

--- swe/backend/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from backend.models import developer
-from backend.models import doctor
-# Register your models here.
-admin.site.register(developer)
-admin.site.register(doctor)
--- swe/static/js/login.js
+++
@@ -1,4 +0,0 @@
-var message_ele = document.getElementById("msg")
-    setTimeout(function(){ 
-      message_ele.style.display = "none"; 
-    }, 2000);
